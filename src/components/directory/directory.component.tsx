03d1f95412fd090f95d8adfd5dae440648e29fa3
André Aragão <andrearagaoart@gmail.com>
2021-02-02 22:37:30 -0300

    update our menu-items to navigate withRouter High Order Component

--- src/components/directory/directory.component.tsx
+++ src/components/directory/directory.component.tsx
@@ -11,6 +11,7 @@ interface SectionsType {
   imageUrl: string;
   size?: string;
   id: number;
+  linkUrl: string
 }
 
 interface DirectoryState {
@@ -23,29 +24,34 @@ class Directory extends Component<DirectoryProps, DirectoryState> {
       {
         title: 'hats',
         imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-        id: 1
+        id: 1,
+        linkUrl: 'hats'
       },
       {
         title: 'jackets',
         imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-        id: 2
+        id: 2,
+        linkUrl: ''
       },
       {
         title: 'sneakers',
         imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-        id: 3
+        id: 3,
+        linkUrl: ''
       },
       {
         title: 'womens',
         imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
         size: 'large',
-        id: 4
+        id: 4,
+        linkUrl: ''
       },
       {
         title: 'mens',
         imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
         size: 'large',
-        id: 5
+        id: 5,
+        linkUrl: ''
       }
     ]
   }
@@ -54,8 +60,8 @@ class Directory extends Component<DirectoryProps, DirectoryState> {
     return (
       <div className="directory-menu">
         {
-          this.state.sections.map(({ title, imageUrl, id, size }) => (
-            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+          this.state.sections.map(({ id, ...otherSectionProps }) => (
+            <MenuItem key={id} {...otherSectionProps} />
           ))
         }
       </div>
--- src/components/menu-item/menu-item.component.tsx
+++ src/components/menu-item/menu-item.component.tsx
@@ -1,24 +1,30 @@
+import { withRouter, RouteComponentProps } from 'react-router-dom';
+
 import './menu-item.styles.scss';
 
 interface MenuItemProps {
   title: string;
   imageUrl: string;
   size?: string;
+  linkUrl: string
 }
 
-const MenuItem = ({ title, imageUrl, size = ''}: MenuItemProps) => (
-  <div className={`${size} menu-item`} >
-    <div 
+const MenuItem = ({ title, imageUrl, size = '', history, linkUrl, match }: MenuItemProps & RouteComponentProps) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div
       style={{
         backgroundImage: `url(${imageUrl})`
       }}
       className="background-image"
     />
-    <div className="content"> 
+    <div className="content">
       <h1 className="title">{title}</h1>
       <span className="subtitle">Shop now</span>
     </div>
   </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
